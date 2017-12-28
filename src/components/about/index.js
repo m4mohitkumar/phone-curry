64d035dd81d44f7156f2d081ccc91192996f474f
mukundjee <mukundkumarmishra@gmail.com>
2017-12-28 21:22:50 +0530

    added connection with api, and designed the home page

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,8 +7,47 @@ class About extends Component {
       <div className="container-fluid">
         <Row>
           <Col md={12}>
-            <h3>About Page.</h3>
+            <h3>About Phones.</h3>
           </Col>
+
+          <table class="table">
+    <thead>
+      <tr>
+        <th>Phonename</th>
+        <th>Price</th>
+        <th>manufacturer</th>
+        <th>Screen Size</th>
+        <th>Os version</th>
+        <th>Phone detail</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Iphone se</td>
+        <td>17000</td>
+        <td>Apple inc.</td>
+        <td>4 Inch</td>
+        <td>Ios 11</td>
+        <td>good phone</td>
+      </tr>
+      <tr>
+        <td>Yu Yureka Plus</td>
+        <td>10000</td>
+        <td>Yu</td>
+        <td>5.5 Inch</td>
+        <td>Android Kit Kat</td>
+        <td>good phone</td>
+      </tr>
+      <tr>
+        <td>Iphone 6</td>
+        <td>20000</td>
+        <td>Apple inc</td>
+        <td>4.7 Inch</td>
+        <td>Ios 11</td>
+        <td>good phone</td>
+      </tr>
+    </tbody>
+  </table>
         </Row>
       </div>
     );
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import {Row, Col} from 'react-bootstrap';
+import axios from 'axios';
 import logo from './logo.svg';
+import coverimage from './cover-image.jpg';
+
 import './index.css';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      phone_list : [
+        {name : 'iphone'}
+      ]
+    };
+  }
   render() {
 
     function formatName(user) {
@@ -28,26 +39,44 @@ class Home extends Component {
       </h1>
     );
 
+    let url = "http://127.0.0.1:8000/Phone_info/";
+    axios.get(url).then(res => {
+      let phone_list = res.data.map(function (data) {
+        return data;
+      });
+      this.setState({
+        phone_list : phone_list
+      });
+    }).catch(err => {
+      console.error(err);
+    });
+
     return (
       <div className="container-fluid">
+
+
         <Row>
           <Col md={12}>
             <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to Phone Curry</h1>
-              </header>
-              <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-              </p>
+                <h1>Decide which phone to buy..in 5 minutes!</h1>
+                <p style={{fontStyle: 'italic',fontSize: '17px', color: 'grey'}}>no-nonsense phone buying advice</p>
+                <img src={coverimage} className="" alt="logo" style={{maxWidth:"100%"}} />
               <hr />
-              <div>
-                {element}
-              </div>
             </div>
+            <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">About Site</button>
+            <div id="demo" class="collapse">
+              <p>
+                This site gives the best Information about the latest Phones available in the Market with their best
+                <code>Features</code>, their <code>Review</code> and also the <code>Minimum Price</code> available for the Latest Phones.
+              </p>
+            </div>
+            <h1>
+              {/* {this.state.phone_list[0].name} */}
+            </h1>
           </Col>
         </Row>
       </div>
+
     );
   }
 }
